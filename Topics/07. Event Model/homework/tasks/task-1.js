957490fed9569bbd313eeb2ddcb201afa035b93d
MilenIvanov <ivanovvmilen@gmail.com>
2019-02-22 14:44:05 +0200

    fix

--- Topics/07. Event Model/homework/tasks/task-1.js
+++ Topics/07. Event Model/homework/tasks/task-1.js
@@ -42,7 +42,7 @@ function solve() {
         for (let i = 0; i < selected.length; i++) {
             if(selected[i].className === "button"){
                 selected[i].innerHTML = "hide";
-                selected[i].addEventListener("click", ContentModifier);
+                selected[i].addEventListener("click", ContentModifier); 
             }
             
         }
@@ -50,14 +50,11 @@ function solve() {
         function ContentModifier(event) {
             let selectedContent;
             let Nodes = element.childNodes;
-            
+
             for (let i = 0; i < Nodes.length; i++) {
                 if(Nodes[i] === event.target){
                     for (let n = i + 1 ; n < Nodes.length; n++) {
-                        if(Nodes[n].className === "button"){
-                            return;
-                        }
-                        else if(Nodes[n].className === "content"){
+                if(Nodes[n].className === "content"){
                             selectedContent = Nodes[n];
                             break;
                         }
@@ -67,12 +64,12 @@ function solve() {
             }
 
             if(!(selectedContent.offsetWidth > 0 && selectedContent.offsetHeight > 0)){
-                selectedContent.style.display = "block";
                 event.target.innerHTML = "hide";
+                selectedContent.style.display = "block";
                 return;
             }
-            selectedContent.style.display = "none";
             event.target.innerHTML = "show";
+            selectedContent.style.display = "none";
 
         }
     };
