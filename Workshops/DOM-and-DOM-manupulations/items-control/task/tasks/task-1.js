037589841125d6de9d331899fc99854ea33a1602
MilenIvanov <ivanovvmilen@gmail.com>
2019-02-23 14:15:41 +0200

    image-control taks complete

--- Workshops/DOM-and-DOM-manupulations/items-control/task/tasks/task-1.js
+++ Workshops/DOM-and-DOM-manupulations/items-control/task/tasks/task-1.js
@@ -2,165 +2,87 @@
 
 function solve() {
     return function(selector, isCaseSensitive) {
-        var element = selector,
-            fragment,
+            let addText,addInput,button,list,addElements,root,rootFragment,searchElements,searchText,searchInput,Result;
 
-            addControls,
-            labelAdd,
-            tbAdd,
-            btnAdd,
+            addText = document.createElement("label");
+            addInput = document.createElement("input");
+            button = document.createElement("button");
+            addElements = document.createElement("div");
+            rootFragment = document.createDocumentFragment();
+            list = document.createElement("div");
+            searchText = document.createElement("label");
+            searchInput = document.createElement("input");
+            searchElements = document.createElement("div"); 
+            Result = document.createElement("div");
 
-            searchControls,
-            labelSearch,
-            tbSearch,
+            root = document.querySelector(selector);
+            root.setAttribute("class","items-control");
+            addText.innerHTML = "Enter text";
+            addInput.setAttribute("type","text");
+            button.setAttribute("class","button");
+            button.innerHTML = "Add";
+            button.addEventListener("click",Add)
+            addElements.setAttribute("class","add-controls")
+            searchText.innerHTML = "Search:";
+            searchInput.addEventListener("change",Search);
+            searchElements.setAttribute("class","search-controls");
+            list.setAttribute("class","items-list");
+            Result.setAttribute("class","result-controls");
+            
 
-            resultControls,
-            listResults,
-            listItemTemplate,
-            btnDeleteListItem,
-            textListItem,
-            listItems;
-
-
-        isCaseSensitive = !!isCaseSensitive;
-        if (typeof element === "string") {
-            element = document.querySelector(element);
-        }
-        if (!element || !(element instanceof HTMLElement)) {
-            throw new Error("Invalid HTML element or selector");
-        }
-
-        fragment = document.createDocumentFragment();
-
-        /* Add Controls: START */
-
-        addControls = document.createElement("div");
-        addControls.className = "add-controls";
-
-        labelAdd = document.createElement("label");
-        labelAdd.innerHTML = "Enter text: ";
-
-        tbAdd = document.createElement("input");
-
-        labelAdd.appendChild(tbAdd);
-
-        btnAdd = document.createElement("a");
-        btnAdd.className = "button";
-        btnAdd.addEventListener("click", onAddButtonClick, false);
-        btnAdd.innerHTML = "Add";
-        btnAdd.style.display = "inline-block";
-
-        addControls.appendChild(labelAdd);
-        addControls.appendChild(btnAdd);
-
-        /* Add Controls: END */
+            addElements.appendChild(addText);
+            addElements.appendChild(addInput);
+            addElements.appendChild(button);
+            Result.appendChild(list);
+            searchElements.appendChild(searchText);
+            searchElements.appendChild(searchInput);
+            rootFragment.appendChild(addElements);
+            rootFragment.appendChild(searchElements);
+            rootFragment.appendChild(Result);
+            root.appendChild(rootFragment);
 
 
-        /* Search Controls: START */
+            function Add(event) {
+                let item,button,text;
+                item = document.createElement("div");
+                button = document.createElement("button");
+                text = document.createElement("label");
 
-        searchControls = document.createElement("div");
-        searchControls.className = "search-controls";
+                item.setAttribute("class","list-item");
+                text.innerHTML = addInput.value;
+                addInput.value = "";
+                button.addEventListener("click",Remove);
+                button.setAttribute("class","button");
+                button.innerHTML = "X";
+                item.appendChild(text);
+                item.appendChild(button);
+                list.appendChild(item);
 
-        labelSearch = document.createElement("label");
-        labelSearch.innerHTML = "Search:";
-
-        tbSearch = document.createElement("input");
-
-        tbSearch.addEventListener("input", onSearchTextboxInput, false);
-
-        labelSearch.appendChild(tbSearch);
-
-        searchControls.appendChild(labelSearch);
-
-        /* Search Controls: END */
-
-        /* Result Controls: START */
-
-        resultControls = document.createElement("div");
-        resultControls.className = "result-controls";
-
-        listResults = document.createElement("ul");
-        listResults.className = "items-list";
-
-        listItemTemplate = document.createElement("li");
-        listItemTemplate.className = "list-item";
-
-        btnDeleteListItem = document.createElement("a");
-        btnDeleteListItem.className = "button button-delete";
-        btnDeleteListItem.innerHTML = "X";
-
-        textListItem = document.createElement("strong");
-        listItemTemplate.appendChild(btnDeleteListItem);
-        listItemTemplate.appendChild(textListItem);
-
-        listResults.addEventListener("click", onListResultClick, false);
-
-
-        resultControls.appendChild(listResults);
-
-        listItems = element.getElementsByClassName("list-item");
-
-        /* Result Controls: END */
-
-        fragment.appendChild(addControls);
-        fragment.appendChild(searchControls);
-        fragment.appendChild(resultControls);
-
-        element.appendChild(fragment);
-        element.className += "items-control";
-
-
-        function onAddButtonClick(ev) {
-            var value = tbAdd.value;
-            tbAdd.value = "";
-
-            textListItem.innerHTML = value;
-
-            listResults.appendChild(listItemTemplate.cloneNode(true));
-        }
-
-        function onSearchTextboxInput() {
-            var i = 0,
-                len,
-                text,
-                pattern = tbSearch.value;
-            if (!isCaseSensitive) {
-                pattern = pattern.toLowerCase();
-            }
-
-            for (i = 0, len = listItems.length; i < len; i += 1) {
-                text = listItems[i].getElementsByTagName("strong")[0].innerHTML;
-                if (!isCaseSensitive) {
-                    text = text.toLowerCase();
-                }
-
-                if (text.indexOf(pattern) < 0) {
-                    listItems[i].style.display = "none";
-                } else {
-                    listItems[i].style.display = "";
+                function Remove(event) {
+                    event.target.parentElement.outerHTML = "";
                 }
             }
-        }
 
-        function onListResultClick(ev) {
-            var btn = ev.target,
-                parent;
-            if (btn.className.indexOf("button-delete") < 0) {
-                return;
+            function Search(event) {
+                debugger;
+                let itemList = list.querySelectorAll(".list-item");
+                for (let i = 0; i < itemList.length; i++) {
+                    itemList[i].style.display = "none";
+                    let parText = itemList[i].childNodes[0];
+                    if(!isCaseSensitive){
+                        if(parText.innerHTML.toLowerCase().includes(event.target.value.toLowerCase())){
+                            itemList[i].style.display = "block";
+                        }
+                    }else {
+                        if(parText.innerHTML.includes(event.target.value)){
+                            itemList[i].style.display = "block";
+                        }
+                    }
+                }
             }
+             
 
-            parent = btn;
-            while (parent && parent.className.indexOf("list-item") < 0) {
-                console.log(parent.nodeName);
-                parent = parent.parentNode;
-            }
 
-            if (!parent) {
-                return;
-            }
-
-            listResults.removeChild(parent);
-        }
     };
 }
 
