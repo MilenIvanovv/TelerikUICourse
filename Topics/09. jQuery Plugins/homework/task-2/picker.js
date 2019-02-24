9a42004deebe81c33d68940ec872ce8007312475
MilenIvanov <ivanovvmilen@gmail.com>
2019-02-24 19:12:22 +0200

    colorPicker task complete

--- Topics/09. jQuery Plugins/homework/task-2/picker.js
+++ Topics/09. jQuery Plugins/homework/task-2/picker.js
@@ -1,3 +1,115 @@
+
+
+
+
 $.fn.colorpicker = function () {
-    
+
+    let element = $(this);
+
+    //Buton
+    let button = $("<img>").attr({
+        id: "btn-picker",
+        src: "./imgs/icon.jpg"
+    });
+    button.click(togglePicker);
+    element.append(button);
+
+    //Template ColorPicker
+    let canvas = $("<canvas>").attr({
+        id:"myCanvas"
+    });
+    let canvasImage = $("<img>").attr({
+        id: "canvasImage",
+        src:"./imgs/color-picker.png"
+    });
+    let exitButton = $("<img>").attr({
+        id: "exit",
+        src: "./imgs/close.png"
+    });
+    let divColorPicker = $("<div>").attr({
+        id: "colorPicker"
+    });
+    let inputHEX = $("<input>").attr({
+        id:"HEX",
+        type:"text",
+        placeholder:"HEX"
+    });
+    let inputRGB = $("<input>").attr({
+        id:"RGB",
+        type:"text",
+        placeholder:"RGB"
+    });
+    let colorPreview = $("<div>").attr({
+        id: "color"
+    });
+    let divColorInfo = $("<div>");
+
+    divColorInfo.append(inputHEX);
+    divColorInfo.append(inputRGB);
+    divColorInfo.append(colorPreview);
+    divColorPicker.append(exitButton);
+    divColorPicker.append(canvas);
+    divColorPicker.append(divColorInfo);
+
+    let ctx;
+    function togglePicker() {
+        if($("#colorPicker").length){
+            close();
+            return;
+        }
+        canvas.click(getColor);
+        exitButton.click(close);
+        inputRGB.change(changeColorbyRGB);
+        inputHEX.change(changeColorbyHEX);
+        element.append(divColorPicker);
+        ctx = canvas[0].getContext("2d");
+        ctx.drawImage(canvasImage[0], 10, 10);
+    }
+
+    function getColor(event) {
+        let data,offsetTop,offsetLeft;
+        offsetTop = event.target.getBoundingClientRect().top;
+        offsetLeft = event.target.getBoundingClientRect().left;
+        data = ctx.getImageData(event.pageX-offsetLeft,event.pageY-offsetTop,1,1).data;
+        inputRGB[0].value = data[0] + "." + data[1] + "." + data[2];
+        inputHEX[0].value = RGBtoHEX(data[0],data[1],data[3]);
+        inputHEX.copy();
+        colorPreview.css("background-color", "rgb(" + data[0] + "," + data[1] + "," + data[2] + ")");
+    }
+
+    function changeColorbyHEX(event) {
+        colorPreview.css("background-color", event.target.value);
+        inputRGB[0].value = hexToRgb(event.target.value);
+        inputHEX.copy;
+    }
+
+    function changeColorbyRGB(event) {
+        let color = event.target.value.replace(".", ",");
+        color = color.replace(".", ",");
+        colorPreview.css("background-color", "rgb(" + color + ")");
+        let colors = color.split(",");
+        inputHEX[0].value = RGBtoHEX(Number(colors[0]),Number(colors[1]),Number(colors[2]));
+        inputHEX.copy();
+    }
+
+
+    $.fn.copy = function() {
+        this[0].select();
+        document.execCommand("copy");
+        console.log("copied to clipboard:",inputHEX[0].value);
+    }
+
+    function RGBtoHEX(r, g, b) { 
+        return '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
+    }
+
+    const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
+
+    function close() {
+        $("#colorPicker").remove();
+    }
 }
