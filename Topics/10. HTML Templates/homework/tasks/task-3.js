5700e9b4312c1ec725ce07e363c06178b5dedf09
MilenIvanov <ivanovvmilen@gmail.com>
2019-02-26 13:53:15 +0200

    HTML Template taks 3

--- Topics/10. HTML Templates/homework/tasks/task-3.js
+++ Topics/10. HTML Templates/homework/tasks/task-3.js
@@ -1,7 +1,16 @@
 function solve(){
   return function(){
     $.fn.listview = function(data){
-      
+        let template = $("#" + this.attr("data-template")).html();
+        var templateScript = Handlebars.compile(template);
+
+
+        data.forEach(element => {
+            var html = templateScript(element);
+            this.append(html);
+        });
+
+        
     };
   };
 }
