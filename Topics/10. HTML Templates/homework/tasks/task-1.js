55778152e9915648deddf17efe12d5be5420d012
MilenIvanov <ivanovvmilen@gmail.com>
2019-02-26 13:21:28 +0200

    Templates Topic taks1,2 (test broken)

--- Topics/10. HTML Templates/homework/tasks/task-1.js
+++ Topics/10. HTML Templates/homework/tasks/task-1.js
@@ -3,7 +3,7 @@
 function solve() {
   
   return function (selector) {
-    var template = ''; /* insert the template here as a string
+    /* insert the template here as a string
         example:
         var template =
           '<ul>' +
@@ -15,7 +15,50 @@ function solve() {
           '</ul>';
     */
 
-    $(selector).html(template);
+   data = {        
+    headers : ['Vendor', 'Model', 'OS'],          
+    items : [{          
+      col1: 'Nokia',            
+      col2: 'Lumia 920',            
+      col3: 'Windows Phone'                      
+    }, {          
+      col1: 'LG',            
+      col2: 'Nexus 5',            
+      col3: 'Android'                      
+    }, {          
+      col1: 'Apple',            
+      col2: 'iPhone 6',                        
+      col3: 'iOS'                      
+    }]          
+  };
+
+  
+   var template = 
+    "<table class=\"items-table\">"+
+        "<thead>"+
+            "<tr>"+
+                "<th>#</th>"+
+                "{{#each headers}}"+
+                    "<th> {{this}}</th>"+
+                "{{/each}}"+
+            "</tr>"+
+        "</thead>"+
+        "<tbody>"+
+            "{{#each items}}"+
+                "<tr>"+
+                    "<td>{{@index}}</td>"+
+                    "<td>{{col1}}</td>"+
+                    "<td>{{col2}}</td>"+
+                    "<td>{{col3}}</td>"+
+                "</tr>"+
+            "{{/each}}"+
+        "</tbody>"+
+    "</table>";
+
+    var templateScript = Handlebars.compile(template);
+    var html = templateScript(data);
+
+    $(selector).append(html);
   };
 };
 
--- Topics/10. HTML Templates/homework/tasks/task-2.js
+++ Topics/10. HTML Templates/homework/tasks/task-2.js
@@ -3,7 +3,7 @@
 function solve() {
   
   return function (selector) {
-    var template = ''; /* insert the template here as a string
+   /* insert the template here as a string
         example:
         var template =
           '<ul>' +
@@ -14,8 +14,44 @@ function solve() {
             '{{/students}}' +
           '</ul>';
     */
+   var data = {
+    animals: [{
+     name: 'Lion',
+     url: 'https://susanmcmovies.files.wordpress.com/2014/12/the-lion-king-wallpaper-the-lion-king-2-simbas-pride-4685023-1024-768.jpg'
+    }, {
+     name: 'Turtle',
+      url: 'http://www.enkivillage.com/s/upload/images/a231e4349b9e3f28c740d802d4565eaf.jpg'
+    }, {
+      name: 'Dog'              
+    }, {
+      name: 'Cat',
+      url: 'http://i.imgur.com/Ruuef.jpg'
+    }, {
+      name: 'Dog Again'              
+    }] 
+  }
+  var defaultUrl ="http://cdn.playbuzz.com/cdn/3170bee8-985c-47bc-bbb5-2bcb41e85fe9/d8aa4750-deef-44ac-83a1-f2b5e6ee029a.jpg";
 
-    $(selector).html(template);
+
+  var template = "<div class\"container\">"+
+        "<h1>Animals</h1>"+
+        "<ul class=\"animals-list\">"+         
+            "{{#each animals}}"+
+            "<li>" +
+                "{{#if url}}" +
+                "<a href=\"{{url}}\">See a {{name}}</a>"+
+                "{{else}}" +
+                "<a href=\"{{defaultUrl}}\">No link for {{name}}</a>"+
+                "{{/if}}" +
+            "</li>"+
+            "{{/each}}"+            
+        "</ul>"+
+    "</div>";
+
+    var templateScript = Handlebars.compile(template);
+    var html = templateScript(data);
+
+    $(selector).append(html);
   };
 };
 
