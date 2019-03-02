ea928744b668ae27f057eba9211f555a1cef251e
MilenIvanov <ivanovvmilen@gmail.com>
2019-03-02 18:56:24 +0200

    sreadsheet taks complete

--- Practical Exams/28-March-2017/Task-2_Spreadsheet/task/task.js
+++ Practical Exams/28-March-2017/Task-2_Spreadsheet/task/task.js
@@ -1,183 +1,191 @@
 function solve() {
-	const A_CHAR_CODE = 'A'.charCodeAt(0);
 
-	const SPREADSHEET_ITEM_CLASS = 'spreadsheet-item';
-	const SPREADSHEET_CELL_CLASS = 'spreadsheet-cell';
-	const SPREADSHEET_HEADER_CLASS = 'spreadsheet-header';
-	const SPREADSHEET_TABLE_CLASS = 'spreadsheet-table';
-	const SELECTED_CLASS = 'selected';
-	const EDITING_CLASS = 'editing';
+	return function(selector, rows, colums) {
+        //If wrong input handlers
 
-	function buildTable(rows, cols) {
-		const $headerCell = $('<th>')
-			.addClass(SPREADSHEET_HEADER_CLASS)
-			.addClass(SPREADSHEET_ITEM_CLASS);
-		const $dataCell = $('<td>')
-			.addClass(SPREADSHEET_CELL_CLASS)
-			.addClass(SPREADSHEET_ITEM_CLASS);
-		$('<input>').appendTo($dataCell);
-		$('<span>').appendTo($dataCell);
+        //Tools
+            //Arguments
+            const ROWS = (rows + 1),
+                COLUMS = (colums + 1);
+            //Classes   
+            let table_class = "spreadsheet-table",
+            item_class = "spreadsheet-item", 
+            header_class = "spreadsheet-header",
+            cell_class = "spreadsheet-cell", 
+            selected_class = "selected", 
+            editing_class = "editing";
+            //Elements
+            let container_$element = $(selector),
+                container_$fragment = $(document.createDocumentFragment());
+                table_$element = $("<table>")
+                .addClass(table_class)
+                .appendTo(container_$fragment);
+                row_$element = $("<tr>");
+                header_cell_$element = $("<th>")
+                .addClass(header_class)
+                .addClass(item_class)
+                .mousedown(cell_mousedown_handler)
+                .mousemove(cell_mousemove_handler)
+                .mouseup(cell_mouseup_handler);
+                cell_$element = $("<td>").append($("<input>").blur(input_blur_hangler)).append($("<span>"))
+                .addClass(cell_class)
+                .addClass(item_class)
+                .mousedown(cell_mousedown_handler)
+                .mousemove(cell_mousemove_handler)
+                .mouseup(cell_mouseup_handler)
+                .dblclick(cell_dblclick_handler);
 
-		const $headerRow = $('<tr>');
-		$headerCell.clone().appendTo($headerRow);
-		const $dataRow = $headerRow.clone();
+            //functions
+            function getCells(startArg,endArg) {
+                let start = {x:startArg.x,y:startArg.y},
+                    end = {x:endArg.x,y:endArg.y};
+                let cells = [];
+                if(end.x < start.x) { 
+                    start.x = [end.x, end.x = start.x][0];//swap values
+                }
 
-		for(let i = 0; i < cols; i += 1) {
-			$headerCell.clone()
-				.text(String.fromCharCode(A_CHAR_CODE + i))
-				.appendTo($headerRow);
-		}
+                if(end.y < start.y) { 
+                    start.y = [end.y, end.y = start.y][0];//swap values
+                }
 
-		const $table = $('<table>').addClass(SPREADSHEET_TABLE_CLASS);
-		$table.append($headerRow);
+                for (let y = start.y; y <= end.y; y++) {
+                    for (let x = start.x; x <= end.x; x++) {
+                        cells.push(table_$element.children().eq(y).children().eq(x));
+                    }
+                }
 
-		for(let i = 0; i < cols; i += 1) {
-			$dataCell.clone()
-				.appendTo($dataRow);
-		}
+                (function getHeaderCells() { //gets header cells within the selected rows and colums
+                    for (let x = start.x; x <= end.x; x++) {
+                        cells.push(table_$element.children().eq(0).children().eq(x));
+                    }
 
-		for(let i = 1; i <= rows; i += 1) {
-			const $row = $dataRow.clone();
-			$row.find('th').text(i);
-			$row.appendTo($table);
-		}
+                    for (let y = start.y; y <= end.y; y++) {
+                            cells.push(table_$element.children().eq(y).children().eq(0));
+                    }
+                })();
 
-		$table.children().each((row, tr) => {
-			$(tr).children().each((col, tc) => {
-				$(tc)
-					.data('row', row)
-					.data('col', col);
-			});
-		});
+                return cells;
+            }
 
-		return $table;
-	}
+            function selectCells(cells) {
+                cells.map(function(cell) {
+                    if(cell.hasClass(selected_class)){
+                        return;
+                    }
+                    cell.addClass(selected_class);
+                })
+            }
 
-	return function(selector, rows, cols) {
-		cols = cols || rows; // Just in case ;P
+            function selectAll() {
+                $("."+cell_class +" , ."+header_class).addClass(selected_class);
+            }
 
-		const $table = buildTable(rows, cols);
+            function deSelectAllCells() {
+                $("."+selected_class).removeClass(selected_class);
+            }
 
-		const selection = (function() {
-			let startRow, startCol,
-				stopRow, stopCol,
-				isClicked = false;
+        //Build
+            (function tableBuild() {
+                for(let y = 0; y < ROWS;y ++){ //rows
+                    let new_row_$element = row_$element.clone();
+                    new_row_$element.appendTo(table_$element);
+                    for(let x = 0; x < COLUMS; x ++){ //cells
+                        if(y === 0 || x === 0){//header cells
+                        let new_header_cell_$element = header_cell_$element.clone(true,true);
+                            new_row_$element.append(new_header_cell_$element);
+                            if(y === 0 && x !== 0){//A,B,C..
+                                new_header_cell_$element.text(String.fromCharCode(64 + x));
+                            }
+                            if(y !== 0 && x === 0){//1,2,3..
+                                new_header_cell_$element.text(y);
+                            }
+                            continue;
+                        }
+                        new_row_$element.append(cell_$element.clone(true,true));//other cells
+                    }
+                }
+            })();
 
-			const $cellMatrix = Array.from($table.children())
-				.map(tr => Array.from(tr.children).map($));
+            //Appending table
+            container_$element.append(container_$fragment);
+            
 
-			function selectRectangle(startRow, startCol, stopRow, stopCol) {
-				$table.find('.' + SELECTED_CLASS).removeClass(SELECTED_CLASS);
+        //Events
+        let cell_mousedown_position = {},
+            cell_mousemove_position = {},
+            isSelecting = false;
 
-				if(startRow === 0) {
-					stopRow = rows;
-				}
-				if(startCol === 0) {
-					stopCol = cols;
-				}
-				if(stopRow === 0) {
-					stopRow = 1;
-				}
-				if(stopCol === 0) {
-					stopCol = 1;
-				}
+        function cell_mousedown_handler(event) {
+            let cells,
+                targetCell = $(event.target);
 
-				if(startRow > stopRow) {
-					[startRow, stopRow] = [stopRow, startRow];
-				}
-				if(startCol > stopCol) {
-					[startCol, stopCol] = [stopCol, startCol];
-				}
+            isSelecting = true;
 
-				if(startRow > 0) {
-					for(let col = startCol || 1; col <= stopCol; col += 1) {
-						$cellMatrix[0][col].addClass(SELECTED_CLASS);
-					}
-				}
-				if(startCol > 0) {
-					for(let row = startRow || 1; row <= stopRow; row += 1) {
-						$cellMatrix[row][0].addClass(SELECTED_CLASS);
-					}
-				}
+            deSelectAllCells();
+            cell_mousedown_position.x = targetCell.index();
+            cell_mousedown_position.y = targetCell.parent().index();
+            cells = getCells(cell_mousedown_position,cell_mousedown_position);
+            if(targetCell.hasClass(header_class)){
+                if(cell_mousedown_position.x === 0 && cell_mousedown_position.y === 0){
+                    selectAll();
+                    return;
+                }
+                if(cell_mousedown_position.x === 0 ){
+                    cells = cells.concat(getCells({x:COLUMS,y:cell_mousedown_position.y},cell_mousedown_position));
+                }
+                if(cell_mousedown_position.y === 0 ){
+                    cells = cells.concat(getCells({x:cell_mousedown_position.x,y:ROWS},cell_mousedown_position));
+                }
+            }
+            selectCells(cells);
+        }
+        function cell_mousemove_handler(event) {
+            let cells,
+                targetCell = $(event.target);
 
-				for(let row = startRow; row <= stopRow; row += 1) {
-					for(let col = startCol; col <= stopCol; col += 1) {
-						$cellMatrix[row][col].addClass(SELECTED_CLASS);
-					}
-				}
-			}
+            if(isSelecting){
+                deSelectAllCells();
 
-			function start(e) {
-				isClicked = true;
-
-				let $target = $(e.target);
-				if(!$target.hasClass(SPREADSHEET_ITEM_CLASS)) {
-					$target = $target.parent();
-				}
-
-				const { row, col } = $target.data();
-				[startRow, startCol] = [row, col];
-				[stopRow, stopCol] = [row, col];
-
-				selectRectangle(startRow, startCol, startRow, startCol);
-			}
-
-			function stop(e) {
-				isClicked = false;
-			}
-
-			function move(e) {
-				if(!isClicked) {
-					return;
-				}
-
-				let $target = $(e.target);
-				if(!$target.hasClass(SPREADSHEET_ITEM_CLASS)) {
-					$target = $target.parent();
-				}
-
-				const { row, col } = $target.data();
-				if(row === stopRow && col === stopCol) {
-					return;
-				}
-				[stopRow, stopCol] = [row, col];
-				selectRectangle(startRow, startCol, stopRow, stopCol);
-			}
-
-			function edit(e) {
-				let $target = $(e.target);
-				if(!$target.hasClass(SPREADSHEET_CELL_CLASS)) {
-					$target = $target.parent();
-				}
-
-				$target.addClass(EDITING_CLASS);
-				const $input = $target.find('input');
-				const $span = $target.find('span');
-				$input.val($span.text());
-				$input.focus();
-
-				$input.one('blur', () => {
-					$span.text($input.val());
-					$target.removeClass(EDITING_CLASS);
-				});
-			}
-
-			return { start, stop, move, edit };
-		}());
-
-		$table.on('mousedown', selection.start);
-		$table.on('mouseup', selection.stop);
-		$table.on('mousemove', selection.move);
-		$table.on('dblclick', selection.edit);
-
-		// Not required, just for convenience
-		$table.on('keypress', function(e) {
-			if(e.key === 'Enter') {
-				e.target.blur();
-			}
-		});
-
-		$(selector).append($table);
+                cell_mousemove_position.x = targetCell.index();
+                cell_mousemove_position.y = targetCell.parent().index();
+                cells = getCells(cell_mousedown_position,cell_mousemove_position);
+                if(targetCell.hasClass(header_class)){
+                    if(cell_mousemove_position.x === 0 ){
+                        cells = cells.concat(getCells({x:COLUMS,y:cell_mousedown_position.y},cell_mousemove_position));
+                    }
+                    if(cell_mousemove_position.y === 0 ){
+                        cells = cells.concat(getCells({x:cell_mousedown_position.x,y:ROWS},cell_mousemove_position));
+                    }
+                    if(cell_mousedown_position.x === 0 && cell_mousedown_position.y){
+                        selectAll();
+                    }
+                }
+                selectCells(cells);
+            }
+        }
+        function cell_mouseup_handler(event){
+            isSelecting = false;
+        }
+        function cell_dblclick_handler(event) {
+            let $cell = $(event.target);
+            if($cell.prop("tagName") !== "TD"){
+                $cell = $cell.parent();
+            }
+            let cellInfo = $cell.find("span").text();
+            $cell.addClass(editing_class);
+            $cell.find("input").css("display","block");
+            $cell.find("input").val(cellInfo);
+            $cell.find("span").text("");
+            $cell.find("input").focus();
+        }
+        function input_blur_hangler(event) {
+            let $input = $(event.target),
+                inputInfo = $input.val();
+            $input.parent().removeClass(editing_class);
+            $input.parent().find("span").text(inputInfo);
+            $input.val("");
+            $input.css("display","none");
+        }
 	};
 }
 
@@ -186,3 +194,4 @@ function solve() {
 if(typeof module !== 'undefined') {
 	module.exports = solve;
 }
+
